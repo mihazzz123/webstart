458eb9848c22965f1007ba4f7877983c1e9c5155
mihazzz <m.dokuchaev73@gmail.com>
2019-03-16 16:00:35 +0300

    Установил JS-Min

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var htmlmin = require('gulp-htmlmin');
 var tinypng = require('gulp-tinypng-compress');
+var jsmin = require('gulp-jsmin');
+var rename = require('gulp-rename');
 
 
 gulp.task('minify-css', function(done) {
@@ -14,38 +16,46 @@ gulp.task('minify-css', function(done) {
   done();
 });
 
-gulp.task('move-js', function(done) {
-  return gulp.src('./src/js/*.js')
-  .pipe(gulp.dest('dist/js/'))
-
-  done();
-});
 
 gulp.task('htmlmin', function(done) {
   return gulp.src('./src/*.html')
   .pipe(htmlmin({ collapseWhitespace: true }))
   .pipe(gulp.dest('dist'))
-
+  
   done();
 });
 
 gulp.task('fonts', function(done) {
   return gulp.src('./src/fonts/**/*')
   .pipe(gulp.dest('dist/fonts/'))
-
+  
   done();
 });
 
 gulp.task('tinypng', function (done) {
   return gulp.src('./src/img/**/*.{png,jpg,jpeg}')
-      .pipe(tinypng({
-          key: '98Qy10jrW6zRszWbgLCh02Lzp5qkxgLf',
-      }))
-      .pipe(gulp.dest('dist/img/'));
+  .pipe(tinypng({
+    key: '98Qy10jrW6zRszWbgLCh02Lzp5qkxgLf',
+  }))
+  .pipe(gulp.dest('dist/img/'));
   done();
 });
 
-gulp.task('default', gulp.parallel('minify-css', 'move-js', 'fonts', 'tinypng', 'htmlmin', function(done) {
+gulp.task('min-js', function () {
+    return gulp.src(["./src/js/*.js", "!./src/js/*.min.js"])
+    .pipe(jsmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist/js/'))
+});
+  
+gulp.task('move-js', function(done) {
+  return gulp.src("./src/js/*.min.js")
+  .pipe(gulp.dest('dist/js/'))
+
+  done();
+});
+  
+gulp.task('default', gulp.parallel('minify-css', 'min-js', 'move-js', 'fonts', 'tinypng', 'htmlmin', function(done) {
   // do more stuff
   done();
 }));
